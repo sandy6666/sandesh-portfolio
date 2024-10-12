c55e0163b7d1e80958949063c96a6096493a3ea2
Sandesh S <sandesh.s@codilar.com>
2024-10-12 19:20:11 +0530

    added metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Menu from "@/components/generic/Menu";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={urbanist.className}>
       <Menu/>
       {children}
+      <SpeedInsights />
       </body>
     </html>
   );
