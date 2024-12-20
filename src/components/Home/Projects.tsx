4a8a6e33472a23bfe2e23675e7f134b833bd3035
Sandesh S <reachoutsandesh@gmail.com>
2024-12-20 16:58:51 +0530

    added new project

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -16,20 +16,21 @@ const Projects = () => {
         </p>
         <div className='projects mt-20'>
           <div className='flex flex-wrap justify-center gap-4'>
-            <div className='bg-gray-900 text-white p-6 rounded-lg w-[300px] sm:w-[400px] md:w-[550px] lg:w-[640px] h-[400px] sm:h-[500px] lg:h-[660px] mx-auto shadow-md border-gray-600 hover:border-white border-2 transition-colors duration-500'>
+            <div
+                className='bg-gray-900 text-white p-6 rounded-lg w-[300px] sm:w-[400px] md:w-[550px] lg:w-[640px] h-[400px] sm:h-[500px] lg:h-[660px] mx-auto shadow-md border-gray-600 hover:border-white border-2 transition-colors duration-500'>
               {/* Image Section */}
               <div className='relative w-full h-[85%] mb-4'>
                 <Link
-                  href='https://3d-portfolio-gilt-delta.vercel.app/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                    href='https://3d-portfolio-gilt-delta.vercel.app/'
+                    target='_blank'
+                    rel='noopener noreferrer'
                 >
                   <Image
-                    src='/portfolio-framer.png'
-                    alt='Project Image'
-                    layout='fill'
-                    objectFit='cover'
-                    className='rounded-lg'
+                      src='/portfolio-framer.png'
+                      alt='Project Image'
+                      layout='fill'
+                      objectFit='cover'
+                      className='rounded-lg'
                   />
                 </Link>
               </div>
@@ -38,16 +39,16 @@ const Projects = () => {
               <div className='flex justify-between items-center'>
                 <div>
                   <Link
-                    href='https://3d-portfolio-gilt-delta.vercel.app/'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                      href='https://3d-portfolio-gilt-delta.vercel.app/'
+                      target='_blank'
+                      rel='noopener noreferrer'
                   >
                     <h2 className='text-2xl font-bold mb-2'>3D Portfolio</h2>
                   </Link>
                   <Link
-                    href='https://3d-portfolio-gilt-delta.vercel.app/'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                      href='https://3d-portfolio-gilt-delta.vercel.app/'
+                      target='_blank'
+                      rel='noopener noreferrer'
                   >
                     <p className='text-gray-400'>
                       Build in React using Three.js
@@ -58,19 +59,72 @@ const Projects = () => {
                 {/* Arrow Icon */}
                 <div>
                   <Link
-                    href='https://3d-portfolio-gilt-delta.vercel.app/'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                      href='https://3d-portfolio-gilt-delta.vercel.app/'
+                      target='_blank'
+                      rel='noopener noreferrer'
                   >
                     <button className='bg-gray-800 p-3 rounded-full hover:bg-white transition-colors duration-500'>
-                      <ArrowUpRight className='' />
+                      <ArrowUpRight className=''/>
                     </button>
                   </Link>
                 </div>
               </div>
             </div>
-          </div>
-          <div></div>
+              <div
+                className='bg-gray-900 text-white p-6 rounded-lg w-[300px] sm:w-[400px] md:w-[550px] lg:w-[640px] h-[400px] sm:h-[500px] lg:h-[660px] mx-auto shadow-md border-gray-600 hover:border-white border-2 transition-colors duration-500'>
+              {/* Image Section */}
+              <div className='relative w-full h-[85%] mb-4'>
+                <Link
+                    href='https://type-speed-1873.netlify.app/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                  <Image
+                      src='/typespeed.png'
+                      alt='Project Image'
+                      layout='fill'
+                      objectFit='cover'
+                      className='rounded-lg'
+                  />
+                </Link>
+              </div>
+
+              {/* Title and Description */}
+              <div className='flex justify-between items-center'>
+                <div>
+                  <Link
+                      href='https://type-speed-1873.netlify.app/'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                  >
+                    <h2 className='text-2xl font-bold mb-2'>Test your typing speed</h2>
+                  </Link>
+                  <Link
+                      href='https://type-speed-1873.netlify.app/'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                  >
+                    <p className='text-gray-400'>
+                      Build using vanilla JS
+                    </p>
+                  </Link>
+                </div>
+
+                {/* Arrow Icon */}
+                <div>
+                  <Link
+                      href='https://type-speed-1873.netlify.app/'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                  >
+                    <button className='bg-gray-800 p-3 rounded-full hover:bg-white transition-colors duration-500'>
+                      <ArrowUpRight className=''/>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            </div>
         </div>
       </div>
     </section>
